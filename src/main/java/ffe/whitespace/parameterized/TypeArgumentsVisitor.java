92ed1e8b48cabcf12c58a64d607400b7b7485053
oguna <nao.lk.118@gmail.com>
2016-09-20 11:43:16 +0900

    TypeArgumentの実装を改良

--- src/main/java/ffe/whitespace/parameterized/TypeArgumentsVisitor.java
+++ src/main/java/ffe/whitespace/parameterized/TypeArgumentsVisitor.java
@@ -5,13 +5,12 @@ import ffe.Token;
 import ffe.TokenSequence;
 import ffe.whitespace.Direction;
 import ffe.whitespace.WhiteSpaceVisitor;
-import org.eclipse.jdt.core.dom.ASTNode;
-import org.eclipse.jdt.core.dom.MethodInvocation;
-import org.eclipse.jdt.core.dom.Type;
+import org.eclipse.jdt.core.dom.*;
 import org.eclipse.jdt.core.formatter.DefaultCodeFormatterConstants;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.eclipse.jdt.internal.compiler.parser.TerminalTokens.*;
 
@@ -21,24 +20,108 @@ public class TypeArgumentsVisitor extends WhiteSpaceVisitor {
         super(tokenSequence, featureCollector);
     }
 
-    @Override
-    public boolean visit(MethodInvocation node) {
-        if (node.typeArguments().size() > 0) {
-            Token less = tokenSequence.searchForwardInNode(TokenNameLESS, node);
-            collectFeature(DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_BEFORE_OPENING_ANGLE_BRACKET_IN_TYPE_ARGUMENTS, less, Direction.BEFORE);
-            collectFeature(DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_AFTER_OPENING_ANGLE_BRACKET_IN_TYPE_ARGUMENTS, less, Direction.AFTER);
-            Token closing = tokenSequence.searchForwardAfterNode(Arrays.asList(TokenNameGREATER ,TokenNameRIGHT_SHIFT) , (ASTNode) node.typeArguments().get(node.typeArguments().size() - 1));
-            collectFeature(DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_BEFORE_CLOSING_ANGLE_BRACKET_IN_TYPE_ARGUMENTS, closing, Direction.BEFORE);
-            collectFeature(DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_AFTER_CLOSING_ANGLE_BRACKET_IN_TYPE_ARGUMENTS, closing, Direction.AFTER);
+    private void handleTypeArguments(List<Type> typeArguments) {
+        if (typeArguments.isEmpty()) {
+            return;
         }
-        if (node.typeArguments().size() > 1) {
-            for (int i = 0; i < node.typeArguments().size() - 1; i++) {
-                Type type = (Type)node.typeArguments().get(i);
-                Token comma = tokenSequence.searchForwardAfterNode(TokenNameCOMMA, type);
-                collectFeature(DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_BEFORE_COMMA_IN_TYPE_ARGUMENTS, comma, Direction.BEFORE);
-                collectFeature(DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_AFTER_COMMA_IN_TYPE_ARGUMENTS, comma, Direction.AFTER);
+        handleTokenBefore(typeArguments.get(0), TokenNameLESS,
+                DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_BEFORE_OPENING_ANGLE_BRACKET_IN_TYPE_ARGUMENTS,
+                DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_AFTER_OPENING_ANGLE_BRACKET_IN_TYPE_ARGUMENTS);
+        //handleTokenAfter(typeArguments.get(typeArguments.size() - 1), TokenNameGREATER,
+        //        DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_BEFORE_CLOSING_ANGLE_BRACKET_IN_TYPE_ARGUMENTS,
+        //        DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_AFTER_CLOSING_ANGLE_BRACKET_IN_TYPE_ARGUMENTS);
+        handleCommas(typeArguments,
+                DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_BEFORE_COMMA_IN_TYPE_ARGUMENTS,
+                DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_BEFORE_COMMA_IN_TYPE_ARGUMENTS);
+
+    }
+
+    private void handleCommas(List<? extends ASTNode> nodes, String spaceBefore, String spaceAfter) {
+        if (spaceBefore != null || spaceAfter != null) {
+            for (int i = 0; i <  nodes.size(); i++) {
+                handleTokenBefore(nodes.get(i), TokenNameCOMMA, spaceBefore, spaceAfter);
             }
         }
-        return super.visit(node);
     }
+
+    private void handleTokenAfter(ASTNode node, int tokenType, String spaceBefore, String spaceAfter) {
+        if (spaceBefore != null || spaceAfter != null) {
+            Token token = tokenSequence.searchForwardAfterNode(tokenType, node);
+            handleToken(token, spaceBefore, spaceAfter);
+        }
+    }
+
+    private void handleTokenBefore(ASTNode node, int tokenType, String spaceBefore, String spaceAfter) {
+        if (spaceBefore != null || spaceAfter != null) {
+            Token token = tokenSequence.searchBackwardBeforeNode(tokenType, node);
+            handleToken(token, spaceBefore, spaceAfter);
+        }
+    }
+
+    private void handleToken(Token token, String spaceBefore, String spaceAfter) {
+        if (spaceBefore != null) {
+            collectFeature(spaceBefore, token, Direction.BEFORE);
+        }
+        if (spaceAfter != null) {
+            collectFeature(spaceAfter, token, Direction.AFTER);
+        }
+    }
+
+    @Override
+    public boolean visit(MethodInvocation node) {
+        handleTypeArguments(node.typeArguments());
+        return true;
+    }
+
+    @Override
+    public boolean visit(SuperMethodInvocation node) {
+        handleTypeArguments(node.typeArguments());
+        return true;
+    }
+
+    @Override
+    public boolean visit(ClassInstanceCreation node) {
+        List<Type> typeArguments = node.typeArguments();
+        handleTypeArguments(typeArguments);
+        return true;
+    }
+
+    @Override
+    public boolean visit(ConstructorInvocation node) {
+        handleTypeArguments(node.typeArguments());
+        return true;
+    }
+
+    @Override
+    public boolean visit(SuperConstructorInvocation node) {
+        handleTypeArguments(node.typeArguments());
+        return true;
+    }
+
+    @Override
+    public boolean visit(TypeMethodReference node) {
+        handleTypeArguments(node.typeArguments());
+        return true;
+    }
+
+    @Override
+    public boolean visit(ExpressionMethodReference node) {
+        handleTypeArguments(node.typeArguments());
+        return true;
+    }
+
+    @Override
+    public boolean visit(SuperMethodReference node) {
+        handleTypeArguments(node.typeArguments());
+        return true;
+    }
+
+    @Override
+    public boolean visit(CreationReference node) {
+        handleTypeArguments(node.typeArguments());
+        return true;
+    }
+
+
+
 }
